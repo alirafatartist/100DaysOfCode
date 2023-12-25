e9af10c9395df412d80e8386fd85bd2a4ce10454
alirafatartist <lraft636@gmail.com>
2023-12-25 02:52:27 +0200

    Day2

--- Javascript/Day1/Learn JavaScript by Building a Role Playing Game Freecodecamp/index.js
+++ Javascript/Day1/Learn JavaScript by Building a Role Playing Game Freecodecamp/index.js
@@ -15,5 +15,4 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-
 // step 35
