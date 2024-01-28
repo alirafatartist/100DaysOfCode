5c89fa0a8e85bc1efff35d4946897a767cde0e95
Ali Rafat <lraft636@gmail.com>
2024-01-28 11:26:28 +0200

    Commit

---
+++ implementions/Generate Random Password/README.md
@@ -0,0 +1,5 @@
+For Preview Link 👉
+https://alirafatartist.github.io/100DaysOfCode/implementions/Generate%20Random%20Passwords/
+
+# Generate-Random-Passwords
+![Alt text](image.png)
---
+++ implementions/Generate Random Password/index.js
@@ -0,0 +1,28 @@
+let copyBtn = document.querySelector(".fa");
+let input = document.getElementById("input");
+// generate password function
+function genpass(length = 16) {
+  const characters =
+    "!\"/#/$%&'()*+,-./:;<=>?@[]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    let randomValue = characters[Math.floor(Math.random() * characters.length)];
+    password += randomValue;
+  }
+
+  input.value = password;
+  copyBtn.classList.replace("fa-check", "fa-clone");
+  copyBtn.classList.replace("green", "fa-clone");
+  copyBtn.innerText = "";
+  copyBtn.style.fontFamily = "";
+}
+// copy password function
+async function copyPassword() {
+  navigator.clipboard.writeText(input.value);
+  copyBtn.classList.replace("fa-clone", "fa-check");
+  await new Promise((test) => setTimeout(test, 1000));
+  copyBtn.innerText = "Copied";
+  copyBtn.classList.replace("fa-check", "green");
+  copyBtn.style.fontFamily = "Tahoma";
+}
