c2cf6905271f5fdfe6b40cf49a189fe39770abed
Ali Rafat <lraft636@gmail.com>
2024-03-16 23:23:50 +0200

    Finsh Header & Home & Menu Sections & Js

--- implementions/Restaurant Website/scripts/main.js
+++ implementions/Restaurant Website/scripts/main.js
@@ -1,6 +1,7 @@
 const logo = document.querySelector(".logo");
 const menu = document.querySelector(".fa-bars");
 const links = document.querySelector("nav ul");
+// const img = document.querySelector("nav ul");
 
 function changeimgsrc() {
   if (window.innerWidth <= 768) {
@@ -20,4 +21,23 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("fa-xmark");
   menu.classList.toggle("rotate");
   links.classList.toggle("show");
+});
+
+// scroll reveal
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+ScrollReveal().reveal(".home img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".home .content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".home .content p", {
+  ...scrollRevealOption,
+  delay: 1000,
 });
